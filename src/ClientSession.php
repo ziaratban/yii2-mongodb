1f9b25692ca205b99eea01111b873dd170a1a784
Abolfazl <abolfazl.ziaratban@gmail.com>
2023-03-22 16:37:26 +0430

    Update ClientSession.php

--- src/ClientSession.php
+++ src/ClientSession.php
@@ -135,6 +135,7 @@ class ClientSession extends \yii\base\BaseObject
             Yii::debug('Starting mongodb session ...', __METHOD__);
         }
         $db->trigger(Connection::EVENT_START_SESSION);
+        $db->open();
         $newSession = new self([
             'db' => $db,
             'mongoSession' => $db->manager->startSession($sessionOptions),
