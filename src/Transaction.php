530d841ddae77d99ea33e59947607d7f3de58868
Abolfazl <abolfazl.ziaratban@gmail.com>
2023-03-28 06:16:45 +0330

    Update Transaction.php

--- src/Transaction.php
+++ src/Transaction.php
@@ -43,6 +43,11 @@ class Transaction extends \yii\base\BaseObject
     public $clientSession;
 
     /**
+     * @var Exception|Error the last exception or error thrown that occurred during the call to the run() method
+     */
+    public $lastRunError;
+
+    /**
      * @var mixed your custom data
      */
     public $data;
@@ -198,6 +203,7 @@ class Transaction extends \yii\base\BaseObject
             return $query();
         }
         catch(\Exception|\Error $e) {
+            $this->lastRunError = $e;
             if($throw)
                 throw $e;
             if(!YII_ENV_PROD) {
