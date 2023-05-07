77c309be8396ed006d80740973b42fe7890f6608
Abolfazl <abolfazl.ziaratban@gmail.com>
2023-05-07 17:39:52 +0330

    Update ActiveRecord.php

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -891,7 +891,7 @@ abstract class ActiveRecord extends BaseActiveRecord
 
         $set = [];
         foreach (is_array($lockFieldNames) ? $lockFieldNames : [$lockFieldNames] as $key => $value) {
-            if(is_string($value)) {
+            if(is_int($key)) {
                 $set["_$value"] = new ObjectId;
             }
             else {
